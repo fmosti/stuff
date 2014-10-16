fe7cc7d16d3979045b2ed33cd636973a11848be7
Fabio <fmosti@gmail.com>
2014-10-16 16:08:17 +0100

    ssh stuff

---
+++ vagrant-ssh.ps1
@@ -0,0 +1,4 @@
+if (!(Test-Path "C:\Users\vagrant\.ssh\authorized_keys")) {
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub', 'C:\Users\vagrant\.ssh\authorized_keys')
+}
+
